fd9c1e5b190e203dbd898ef8ae9692c8e3f0131f
Simon Lieschke <simon@lieschke.net>
2021-05-16 16:57:53 -0400

    Add PTZ command to change PTZ camera scenes

--- Program.cs
+++ Program.cs
@@ -1,14 +1,28 @@
 ﻿namespace SceneSwitcher {
     using System;
     using System.IO;
+    using System.Net.Http;
+    using System.Text.Json;
     using System.Threading.Tasks;
     using Microsoft.Office.Interop.PowerPoint;
 
     internal class Program {
         private static readonly Application PowerPoint = new Microsoft.Office.Interop.PowerPoint.Application();
-        private static OBS obs;
+        private static readonly HttpClient HttpClient;
+        private static readonly JsonElement Config;
 
-        private static async Task Main() {
+        private static OBS obs;
+        private static bool testMode;
+
+        static Program() {
+            Console.Write("Reading configuration...");
+            Config = JsonDocument.Parse(File.ReadAllText("config.json")).RootElement;
+            Console.WriteLine(" read");
+        }
+
+        private static async Task Main(string[] args) {
+            testMode = args.Length != 0 && args[0] == "test";
+
             Console.Write("Connecting to PowerPoint...");
             PowerPoint.SlideShowNextSlide += App_SlideShowNextSlide;
             Console.WriteLine(" connected");
@@ -23,7 +37,7 @@
             Console.ReadLine();
         }
 
-        private static void App_SlideShowNextSlide(SlideShowWindow window) {
+        private static async void App_SlideShowNextSlide(SlideShowWindow window) {
             if (window != null) {
                 Console.WriteLine($"Moved to slide number {window.View.Slide.SlideNumber}");
 
@@ -54,7 +68,33 @@
                             Console.WriteLine($"  WARNING: Multiple scene definitions found. I used the first and have ignored \"{line}\"");
                         }
                     }
+
+                    if (line.StartsWith("PTZ:")) {
+                        line = line.Substring(4).Trim();
+
+                        await PTZ(line);
+                    }
                 }
+            }
+        }
+
+        private static async Task PTZ(string line) {
+            Console.WriteLine($"  Switching to PTZ camera scene named \"{line}\"");
+
+            string httpCgiUrl = Config.GetProperty(line).GetString();
+
+            if (testMode) {
+                // For testing at home
+                return;
+            }
+
+            Console.WriteLine(httpCgiUrl);
+
+            try {
+                string responseBody = await HttpClient.GetStringAsync(httpCgiUrl);
+                Console.WriteLine(responseBody);
+            } catch (HttpRequestException ex) {
+                Console.WriteLine($"  ERROR: {ex.Message}");
             }
         }
     }
--- README.md
+++ README.md
@@ -3,13 +3,22 @@ A .NET core based scene switcher that connects to OBS and changes scenes based o
 
 This fork of <https://github.com/shanselman/PowerPointToOBSSceneSwitcher> has been customised and enhanced according to the needs of [Faith Lutheran Church Ottawa](https://faithottawa.ca)'s for livestreaming worship services.
 
-## Usage
-* Set a scene for a slide with 
-```<language>
-OBS:{Scene name as it appears in OBS}
+## Commands
+
+### OBS
+
+Set a scene for a slide with:
+```
+OBS:<Scene name as it appears in OBS>
 ```
 
-Example:
-```<language>
-OBS:Scenename
+### PTZ
+
+Change the PTZ camera scene with:
 ```
+PTZ:<PTZ scene name from config.json>
+```
+
+This uses the scene name as a key to look up a PTZ HTTP-CGI command URL from a `config.json` file in the same directory, and requests the URL.
+
+Running `SceneSwitcher.exe test` will suppress the URLs from being requested, for testing where the PTZ camera is not present on the local network.
